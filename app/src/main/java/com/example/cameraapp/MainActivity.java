ec396730ab396329bc16e939aaac75170a525158
Nancy <chemutain@gmail.com>
2020-04-09 20:49:02 +0300

    Camera App

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.cameraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView myimageview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCapture = findViewById(R.id.bottom);
+        myimageview = findViewById(R.id.imageView);
+
+    }
+
+    public void launchCamera(View view) {
+    }
+}
