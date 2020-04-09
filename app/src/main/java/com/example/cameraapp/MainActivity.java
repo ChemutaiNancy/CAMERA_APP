cafca8a0a1015babffbd032c21e3420ff8087015
Nancy <chemutain@gmail.com>
2020-04-09 21:01:58 +0300

    Capture image

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cameraapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,8 +24,29 @@ public class MainActivity extends AppCompatActivity {
         Button btnCapture = findViewById(R.id.bottom);
         myimageview = findViewById(R.id.imageView);
 
+        if (!hasCamera()){
+            btnCapture.setEnabled(false);
+        }
+    }
+
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            myimageview.setImageBitmap(photo);
+        }
     }
 
     public void launchCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+        //capture image
+
     }
 }
